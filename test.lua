c400c2637183f69076d41073709894b6ad192964
TheWizIcez <154619291+TheWizIcez@users.noreply.github.com>
2023-12-22 23:49:13 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -14,3 +14,17 @@ end
 simulateKeyPress(Enum.KeyCode.E)
 wait(0.5) -- Adjust the delay as needed
 simulateKeyPress(Enum.KeyCode.E)
+
+player.Character:SetPrimaryPartCFrame(CFrame.new(-72.9692993, 66.9621964, 17.4288006, 0.965929627, 0, 0.258804798, 0, 1, 0, -0.258804798, 0, 0.965929627))
+
+-- Wait for a second
+wait(0.5)
+
+local function simulateKeyPress(keyCode)
+    game:GetService("VirtualInputManager"):SendKeyEvent(true, keyCode, false, game)
+end
+
+-- Simulate pressing the "E" key twice with a delay in between
+simulateKeyPress(Enum.KeyCode.E)
+wait(0.5) -- Adjust the delay as needed
+simulateKeyPress(Enum.KeyCode.E)
