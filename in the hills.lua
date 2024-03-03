65afd71b693ec1a3df96dd98c24f41c95f469db5
TheWizIcez <154619291+TheWizIcez@users.noreply.github.com>
2024-03-03 23:07:02 +0700

    in the hills.lua

--- in the hills.lua
+++ in the hills.lua
@@ -49,3 +49,9 @@ while true do
     currentIndex = currentIndex % #activeChests + 1
     wait(0.1)
 end
+
+local bb = game:GetService('VirtualUser')
+game:GetService('Players').LocalPlayer.Idled:Connect(function()
+    bb:CaptureController()
+    bb:ClickButton2(Vector2.new())
+end)
