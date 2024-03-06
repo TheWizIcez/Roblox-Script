e5b54288c7dba3e79798ab756e034822bb487aa3
TheWizIcez <154619291+TheWizIcez@users.noreply.github.com>
2024-03-06 07:31:07 +0700

    in the hills.lua

--- in the hills.lua
+++ in the hills.lua
@@ -3,6 +3,8 @@ local workspace = game.Workspace
 local guiService = game:GetService("GuiService")
 local virtualUser = game:GetService("VirtualUser")
 
+local partName = "TouchedPart"
+
 local function FindPartInGroup(group)
     if group then
         local part = group:FindFirstChild(partName)
@@ -35,27 +37,54 @@ local function TeleportPlayerToPartInFolder(folder)
 end
 
 local function createAntiAFKGui()
-    local gui = Instance.new("ScreenGui")
-    gui.Parent = game.CoreGui
-
-    local frame = Instance.new("Frame")
-    frame.Size = UDim2.new(0, 200, 0, 50)
-    frame.Position = UDim2.new(0.5, -100, 0.5, -25)
-    frame.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-    frame.Parent = gui
-
-    local textLabel = Instance.new("TextLabel")
-    textLabel.Size = UDim2.new(1, 0, 1, 0)
-    textLabel.Position = UDim2.new(0, 0, 0, 0)
-    textLabel.Text = "Anti AFK"
-    textLabel.TextColor3 = Color3.new(1, 1, 1)
-    textLabel.Parent = frame
-end
-
-local function handleAntiAFK()
+    local ba = Instance.new("ScreenGui")
+    ba.Parent = game.CoreGui
+    ba.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    
+    local ca = Instance.new("TextLabel")
+    ca.Parent = ba
+    ca.Active = true
+    ca.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+    ca.Draggable = true
+    ca.Position = UDim2.new(0.698610067, 0, 0.098096624, 0)
+    ca.Size = UDim2.new(0, 370, 0, 52)
+    ca.Font = Enum.Font.SourceSansSemibold
+    ca.Text = "Anti AFK"
+    ca.TextColor3 = Color3.new(0, 1, 1)
+    ca.TextSize = 22
+    
+    local da = Instance.new("Frame")
+    da.Parent = ca
+    da.BackgroundColor3 = Color3.new(0.196078, 0.196078, 0.196078)
+    da.Position = UDim2.new(0, 0, 1.0192306, 0)
+    da.Size = UDim2.new(0, 370, 0, 107)
+    
+    local _b = Instance.new("TextLabel")
+    _b.Parent = da
+    _b.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+    _b.Position = UDim2.new(0, 0, 0.800455689, 0)
+    _b.Size = UDim2.new(0, 370, 0, 21)
+    _b.Font = Enum.Font.Arial
+    _b.Text = "0"
+    _b.TextColor3 = Color3.new(0, 1, 1)
+    _b.TextSize = 20
+    
+    local ab = Instance.new("TextLabel")
+    ab.Parent = da
+    ab.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+    ab.Position = UDim2.new(0, 0, 0.158377, 0)
+    ab.Size = UDim2.new(0, 370, 0, 44)
+    ab.Font = Enum.Font.ArialBold
+    ab.Text = "Status: 1"
+    ab.TextColor3 = Color3.new(0, 1, 1)
+    ab.TextSize = 20
+    
     player.Idled:Connect(function()
         virtualUser:CaptureController()
         virtualUser:ClickButton2(Vector2.new())
+        ab.Text = "0"
+        wait(2)
+        ab.Text = "Status : 1"
     end)
 end
 
@@ -81,4 +110,3 @@ while true do
 end
 
 createAntiAFKGui()
-handleAntiAFK()
